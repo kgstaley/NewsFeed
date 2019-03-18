c82d1d69af6f7507587c277e014fe4fde4e4418b
Kerry Staley <kstaley7819@gmail.com>
2019-03-18 14:19:37 -0700

    initial commit

---
+++ dotnet/NewsFeed.Models/Requests/RegisterUserRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsFeed.Models.Requests
+{
+    public class RegisterUserRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
