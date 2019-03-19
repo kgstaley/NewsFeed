dd10e344b73dc8dd0bcf1572564000ab487aaf32
Kerry Staley <kstaley7819@gmail.com>
2019-03-18 18:29:26 -0700

    added dependencies

--- node/app/server.js
+++ node/app/server.js
@@ -1,5 +1,30 @@
 const express = require("express");
 const { json, urlencoded } = require("body-parser");
 const cookieParser = require("cookie-parser");
-const {config} = require("dotenv");
-const helmet = require('helmet')
+const { config } = require("dotenv");
+const helmet = require("helmet");
+const routes = require("./routes");
+const Responses = require("newsfeed=web-models").Responses;
+
+const app = express();
+var createError = require("http-errors");
+var cors = require("cors");
+const http = require("http").Server(app);
+
+var debugRoute;
+var debugRoutes = [];
+var debugRouteInfo = [];
+
+config();
+
+const port = process.env.PORT || 8080;
+const _isDebugEnv = process.env.SHOW_ROUTES === "true";
+
+app.use(cors({ credentials: true, origin: true }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(helmet());
+
+let apiPrefix = process.env.API_PREFIX || "";
+debugRouteInfo.push("server apiPrefix:", apiPrefix);
---
+++ node/newsfeed-modules/newsfeed-web-models/src/responses/index.js
@@ -0,0 +1,15 @@
+const SuccessResponse = require("./SuccessResponse");
+const ErrorResponse = require("./ErrorResponse");
+const ItemResponse = require("./ItemResponse");
+const ItemsResponse = require("./ItemsResponse");
+const BaseResponse = require("./BaseResponse");
+const Paged = require("./Paged");
+
+module.exports = {
+  SuccessResponse,
+  ErrorResponse,
+  ItemResponse,
+  ItemsResponse,
+  BaseResponse,
+  Paged
+};
