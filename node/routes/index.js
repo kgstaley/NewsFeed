202bc779d6925a08d23395c88f535191dc28c671
Kerry Staley <kstaley7819@gmail.com>
2019-03-19 11:06:53 -0700

    added dependencies

---
+++ node/routes/index.js
@@ -0,0 +1,8 @@
+// this is where router logic goes
+const express = require("express");
+// put controller routes here
+// const birdsController = require("./controllers/birds")
+const router = express.Router();
+// set up routing below
+
+module.exports = router;
