b3220c88931b00a49a329cb267f2051d1a09ccdf
Kerry Staley <kstaley7819@gmail.com>
2019-03-22 14:23:38 -0700

    trying to separate routes and controller

---
+++ node_barebones/controllers/userController.js
@@ -0,0 +1,63 @@
+const express = require("express");
+
+const router = express.Router();
+
+const userServices = require("./services/userServices");
+
+//#region user controller
+router.post("/register", (req, res) => {
+  userServices
+    .insertUser(req.body)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+router.get("/", (req, res) => {
+  userServices
+    .getUsers()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+router.get(`/:id`, (req, res) => {
+  userServices
+    .getUser(req.params.id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+router.put(`/:id/edit`, (req, res) => {
+  userServices
+    .updateUser(req.body)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+router.delete(`/:id/delete`, (req, res) => {
+  userServices
+    .deleteUser(req.params.id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+//#endregion
--- node_barebones/routes/index.js
+++ node_barebones/routes/index.js
@@ -1,64 +1,10 @@
 const express = require("express");
+const feedController = require("../controllers/feedController");
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-const userServices = require("./services/userServices");
+router.use(`/feed`, feedController);
+router.use(`/user`, userController);
 
-//#region user controller
-router.post("/register", (req, res) => {
-  userServices
-    .insertUser(req.body)
-    .then(response => {
-      res.json(response);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
-
-router.get("/users", (req, res) => {
-  userServices
-    .getUsers()
-    .then(response => {
-      res.json(response);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
-
-router.get(`/user/:id`, (req, res) => {
-  userServices
-    .getUser(req.params.id)
-    .then(response => {
-      res.json(response);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
-
-router.put(`/user/:id/edit`, (req, res) => {
-  userServices
-    .updateUser(req.body)
-    .then(response => {
-      res.json(response);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
-
-router.delete(`/user/:id/delete`, (req, res) => {
-  userServices
-    .deleteUser(req.params.id)
-    .then(response => {
-      res.json(response);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
-
-//#endregion
-
+module.exports = router;
