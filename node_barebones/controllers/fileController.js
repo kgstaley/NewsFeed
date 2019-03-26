83568961904800b5c59f0305ca1476a9886abf8d
Kerry Staley <kstaley7819@gmail.com>
2019-03-26 14:34:37 -0700

    nice

--- node_barebones/controllers/fileController.js
+++ node_barebones/controllers/fileController.js
@@ -1,26 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+const fs = require("fs");
 
 const fileService = require("../services/fileServices");
 const AWS = require("aws-sdk");
 const mime = require("mime-types");
 
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
-router.post("/", (req,res)=> {
-    fileService.uploadFile(req.body).then(response=> {
-        res.json(response);
-    }).catch(err=> {
-        res.status(500).send(err);
+router.post("/", (req, res) => {
+  fileService
+    .uploadFile(req.body)
+    .then(response => {
+      res.json(response);
     })
-})
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
 
-const storeFile = (req,res)=> {
-    console.log(req);
-    fileService.storeFile(req.body).then(response=> {
-        res.json(response);
-    }).catch(err=> {
-        res.status(500).send(err);
+const storeFile = (req, res) => {
+  console.log(req);
+  fileService
+    .storeFile(req.body)
+    .then(response => {
+      res.json(response);
     })
-}
+    .catch(err => {
+      res.status(500).send(err);
+    });
+};
 
 module.exports = router;
