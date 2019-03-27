4872440f312252890bdece32a4804bb54b59bcd1
Kerry Staley <kstaley7819@gmail.com>
2019-03-27 10:06:25 -0700

    added uuid dependencies

--- node_barebones/controllers/fileController.js
+++ node_barebones/controllers/fileController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const IncomingForm = require("formidable").IncomingForm;
 
 const fs = require("fs");
 
@@ -13,6 +14,27 @@ AWS.config.update({
 });
 
 router.post("/", (req, res) => {
+  const form = new IncomingForm(); 
+  form.multiples = true; 
+
+  form.on("file", (filed, file)=> {
+    let fileName = ""; 
+    let buffer = null; 
+
+    buffer = fs.readFileSync(file.path);
+    fileName = file.name;
+
+    let upload = {}; 
+    upload.userId = req.user.id;
+    upload.fileName = fileName; 
+
+    const type = mime.contentType(fileName);
+    upload.fileType = type; 
+
+    let key = `newsfeed` 
+  })
+
+
   fileService
     .uploadFile(req.body)
     .then(response => {
